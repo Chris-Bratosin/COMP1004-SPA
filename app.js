adac97f19759141cd0679dc1126516161c07517a
Chris-Bratosin <chris.bratosin@students.plymouth.ac.uk>
2024-02-21 19:14:54 +0000

    added function to mask the password when it is typed in, already does it by default from the label but thought id add this anyways

---
+++ app.js
@@ -0,0 +1,16 @@
+function MaskPassword(pass)
+{
+    let str = ""
+    for (let index = 0; index < pass.length; index++)
+    {
+        str = "*"
+    }
+    return str
+}
+
+
+
+function saveAccount()
+{
+    const accounts = document.getElementById("accounts");
+}
